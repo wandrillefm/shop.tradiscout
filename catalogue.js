c56daa5dce9226377a05b957b047202cfcade522
wandrillefm <98124277+wandrillefm@users.noreply.github.com>
2026-04-28 21:17:43 +0200

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -12,17 +12,17 @@ const COLORS = [
 const SIZES = ['S', 'M', 'L'];
 
 const DESIGNS = [
-  { id: '1', key: 'tradiscout', label: 'Tradiscout c1', types: ["tshirt"], colors: ["blanc","bleu"] },
-  { id: '2', key: 'double-croix', label: 'Double Croix c1', types: ["tshirt","sweat"], colors: ["blanc","bleu"] },
-  { id: '3', key: 'deus-vult', label: 'Deus Vult c1', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '4', key: 'verso-lalto', label: 'Verso l\'Alto c1', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '5', key: 'christus-rex', label: 'Christus Rex c1', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '6', key: 'jpeuxpas', label: 'J\'peux pas j\'ai scout c1', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '7', key: 'mode-grand-jeu', label: 'Mode Grand Jeu c1', types: ["tshirt"], colors: ["blanc","bleu"] },
-  { id: '8', key: 'braises', label: 'Braises Surgelées c1', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '9', key: 'st-michel', label: 'St Michel c2', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '10', key: 'st-jean-marie-vianney', label: 'St Jean-Marie Vianney c2', types: ["sweat","hoodie"], colors: ["blanc","bleu"] },
-  { id: '11', key: 'st-george', label: 'St George c2', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '1', key: 'tradiscout', label: 'Tradiscout', types: ["tshirt"], colors: ["blanc","bleu"] },
+  { id: '2', key: 'double-croix', label: 'Double Croix', types: ["tshirt","sweat"], colors: ["blanc","bleu"] },
+  { id: '3', key: 'deus-vult', label: 'Deus Vult', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '4', key: 'verso-lalto', label: 'Verso l\'Alto', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '5', key: 'christus-rex', label: 'Christus Rex', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '6', key: 'jpeuxpas', label: 'J\'peux pas j\'ai scout', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '7', key: 'mode-grand-jeu', label: 'Mode Grand Jeu', types: ["tshirt"], colors: ["blanc","bleu"] },
+  { id: '8', key: 'braises', label: 'Braises Surgelées', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '9', key: 'st-michel', label: 'St Michel', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '10', key: 'st-jean-marie-vianney', label: 'St Jean-Marie Vianney', types: ["sweat","hoodie"], colors: ["blanc","bleu"] },
+  { id: '11', key: 'st-george', label: 'St George', types: ["tshirt","sweat","hoodie"], colors: ["blanc","bleu"] },
 ];
 
 const GRID_DEFAULT_TYPE  = 'sweat';
